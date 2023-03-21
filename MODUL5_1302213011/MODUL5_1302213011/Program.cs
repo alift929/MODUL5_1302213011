5349e3676ed71316efe06adf734f4fadf55e1191
alift929 <60869026+alift929@users.noreply.github.com>
2023-03-21 17:04:21 +0700

    menambahkan class SimpleDataBase

--- MODUL5_1302213011/MODUL5_1302213011/Program.cs
+++ MODUL5_1302213011/MODUL5_1302213011/Program.cs
@@ -1,2 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+public class SimpleDataBase<T>
+{
+    private List<T> storedData;
+    private List<DateTime> inputDates;
+
+    public SimpleDataBase()
+    {
+        storedData = new List<T>();
+        inputDates = new List<DateTime>();
+    }
+
+    public void addNewDaat<T>(T data)
+    {
+        
+        storedData.Add((dynamic)data);
+        inputDates.Add(DateTime.Now);
+    }
+
+    public void printAllData()
+    {
+        for(int i=0;i<storedData.Count;i++) {
+            Console.WriteLine("Data " + (i + 1) + " berisi: " + storedData[i] + ", yang disimpan pada waktu " + inputDates[i]);
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        SimpleDataBase<string> database = new SimpleDataBase<string>();
+        database.addNewDaat("13");
+        database.addNewDaat("02");
+        database.addNewDaat("21");
+        database.printAllData();
+    }
+}
