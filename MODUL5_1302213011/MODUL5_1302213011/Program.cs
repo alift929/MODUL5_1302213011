de51853e755c9b84a3b1fd9e080ac1565c25a5c7
alift929 <60869026+alift929@users.noreply.github.com>
2023-03-21 16:21:52 +0700

    implementasi-generic-method

--- MODUL5_1302213011/MODUL5_1302213011/Program.cs
+++ MODUL5_1302213011/MODUL5_1302213011/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+public class Penjumlahan
+{
+    public static void jumlahTigaAngka<T>(T num1, T num2, T num3)
+    {
+        Console.WriteLine((dynamic)num1 + num2 + num3);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Penjumlahan.jumlahTigaAngka<float>(13, 02, 21);
+    }
+}
